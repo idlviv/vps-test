87b81547bd9b2e3412b116c33b420208289f90b8
idlviv <idlviv@rambler.ru>
2018-10-09 18:01:49 +0300

    +

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var server = http.createServer(function (req, res) {
   res.end();
 })
 
-server.listen(80, '127.0.0.1', function() {
+server.listen(80, function() {
     console.log('ser', server.address().port);
     console.log('ser', server.address());
 });
