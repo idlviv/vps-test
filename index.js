a0a8d517518d47c3d0c9c442d05343785d5c3922
idlviv <idlviv@rambler.ru>
2018-10-09 17:42:02 +0300

    +

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var http = require('http');
- 
+
 http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World\n');
-}).listen(80, function() {console.log('listening on 80')});
- 
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080, '176.114.8.203', function() {
+    console.log('176.114.8.203:8080');
+});
