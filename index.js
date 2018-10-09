a1ca54022f4f26f667cec2c0fb75209441328e93
idlviv <idlviv@rambler.ru>
2018-10-09 17:43:44 +0300

    +

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World!');
   res.end();
-}).listen(8080, '176.114.8.203', function() {
-    console.log('176.114.8.203:8080');
+}).listen(80, '176.114.8.203', function() {
+    console.log('176.114.8.203:80');
 });
