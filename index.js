c0e1a94da9a249a6c3b516117b87703147f7bd10
idlviv <idlviv@gmail.com>
2018-10-10 12:11:52 +0300

    +

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello!');
-  res.end();
+
 })
 
-server.listen(80, function() {
-    console.log('ser', server.address().port);
+server.listen(8080, function() {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('server port' + server.address().port);
+  res.end();
+    console.log('server port', server.address().port);
     console.log('ser', server.address());
 });
